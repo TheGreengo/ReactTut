6db6293375cd0544bc461f24cacf2e76f570123a
Braden <bradenoptimus@gmail.com>
2023-12-17 22:15:55 -0700

    skajflkjsa

---
+++ reacttut/src/pages/helpers/distentry.js
@@ -0,0 +1,12 @@
+function DistEntry({ thang }) {
+    return (
+        <div>
+            <div></div>
+            <div>{thang.top}</div>
+            <div>{thang.letter}</div>
+            <div>|</div>
+        </div>
+    );
+}
+
+export default DistEntry
--- reacttut/src/pages/helpers/distribution.js
+++ reacttut/src/pages/helpers/distribution.js
@@ -1,14 +1,34 @@
+import DistEntry from "./distentry";
+
 function getDist() {
-    return {
-        
-    };
+    return [
+        {'letter': 'E', 'top': 60},
+        {'letter': 'D-', 'top': 63},
+        {'letter': 'D', 'top': 67},
+        {'letter': 'D+', 'top': 70},
+        {'letter': 'C-', 'top': 73},
+        {'letter': 'C', 'top': 77},
+        {'letter': 'C+', 'top': 80},
+        {'letter': 'B-', 'top': 83},
+        {'letter': 'B', 'top': 87},
+        {'letter': 'B+', 'top': 90},
+        {'letter': 'A-', 'top': 93},
+        {'letter': 'A', 'top': 100}
+    ];
 }
 
 function Distribution() {
+    const grades = getDist();
+
     return (
         <div>
-            <div></div>
-            <div></div>
+            {
+                grades.map(thing => (
+                    (
+                        <DistEntry key={thing.top} thang={thing}/>
+                    )
+                ))
+            }
         </div>
     );
 }
