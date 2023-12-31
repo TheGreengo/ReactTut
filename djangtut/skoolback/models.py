0b346782494550533a9923ec84d406e57b2a0c6f
Braden <bradenoptimus@gmail.com>
2023-12-31 12:59:34 -0700

    akljsldhfjkhas

--- djangtut/skoolback/models.py
+++ djangtut/skoolback/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# So for this I'm thinking that we have:
+# Flashcards (last=integer, id=string, front=string(1000), back=string(1000), starred=bool, deck_id=float)
+# Flashcard decks (id=string, student_id=string)
+# Lessons ()
+# Course ()
+# Assignments (id=string, weight=float, grade=float, student_id=string)
+# Students (id=string, f_name=string(100), l_name=string(100))
