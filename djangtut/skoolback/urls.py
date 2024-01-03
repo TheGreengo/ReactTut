51676f9cfa1825ab1b1db73314ec5d26c0c2226f
Braden <bradenoptimus@gmail.com>
2024-01-02 23:18:53 -0700

    some views added

--- djangtut/skoolback/urls.py
+++ djangtut/skoolback/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("card/<int:card_id>/", views.card, name="card"),
+    path("deck/<int:deck_id>/", views.deck, name="deck"),
+    path("student/<int:student_id>/", views.student, name="student"),
 ]
--- djangtut/skoolback/views.py
+++ djangtut/skoolback/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the skoolback index.")
+
+def card(request, card_id):
+    return HttpResponse("Card: %s" % card_id)
+
+def deck(request, deck_id):
+    return HttpResponse("Deck: %s" % deck_id)
+
+def student(request, student_id):
+    return HttpResponse("Student: %s" % student_id)
