ac6779fb9a8b69f1bc8faa1d680bd4039759dd56
Braden <bradenoptimus@gmail.com>
2024-01-03 22:27:32 -0700

    it now runs a query

--- djangtut/skoolback/views.py
+++ djangtut/skoolback/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import FlashCard
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the skoolback index.")
 
 def card(request, card_id):
-    return HttpResponse("Card: %s" % card_id)
+    kyard = FlashCard.objects.get(id=card_id)
+    return HttpResponse("Front: %s \nBack: %s" % (kyard.front, kyard.back))
 
 def deck(request, deck_id):
     return HttpResponse("Deck: %s" % deck_id)
