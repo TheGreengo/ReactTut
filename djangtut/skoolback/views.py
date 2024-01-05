7488de30f66b4b521eef2699a58f26400ae85246
Braden <bradenoptimus@gmail.com>
2024-01-04 23:10:49 -0700

    nothing really

--- djangtut/skoolback/views.py
+++ djangtut/skoolback/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import FlashCard
+from .models import FlashCard, FlashDeck, Student
 
 
 def index(request):
@@ -11,7 +11,9 @@ def card(request, card_id):
     return HttpResponse("Front: %s \nBack: %s" % (kyard.front, kyard.back))
 
 def deck(request, deck_id):
-    return HttpResponse("Deck: %s" % deck_id)
+    deck = FlashDeck.objects.get(id=deck_id)
+    return HttpResponse("Deck: %s" % deck.name)
 
 def student(request, student_id):
-    return HttpResponse("Student: %s" % student_id)
+    student = Student.objects.get(id=student_id)
+    return HttpResponse("Student: %s" % student.f_name)
